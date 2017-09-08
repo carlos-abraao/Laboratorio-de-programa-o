e69f366f656d00d236fd0aa7199a478a933dd365
carlos-abraao <carlos.abraao.sa@gmail.com>
2017-09-08 12:35:13 -0300

    questão 3 completada.

--- l1q3.c
+++ l1q3.c
@@ -5,15 +5,13 @@ int main(void) {
 
 	//TERCEIRA  QUESTAO
 
-	float seg;
+	float seg, rad, grau, min, s;
+	int h, m;
 	printf("-------------Laboratório de programação 1-------------\n\n");
 	printf("Questão 3:\n");
 
 	printf("Digite a qtde de segundos(real):\n");
-
 	scanf("%f", &seg);
-
-	int h, m;
 	
 	h = seg/3600;
 	seg -= 3600*h;
@@ -22,6 +20,16 @@ int main(void) {
 
 	printf("tempo em h:m:s %02d %02d %05.2f\n", h, m, seg);
 
+	printf("Digite o valor de um ângulo em radianos(real):\n");	
+	scanf("%f", &rad);
+	
+	grau = rad*57.2958;
+	min = grau - (int) grau; grau = (int) grau;
+	min *= 60;
+	s = min - (int) min; min = (int) min;
+	s *= 60;
+
+	printf("Ângulo convertido: %.0f° %02.0f' %02.0f''\n", grau, min, s);
 	
 
 	return 0;
